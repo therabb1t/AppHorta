0427d1759b5a3cc5db09fc7071db9de6479cddd6
Ana Castro <anacabh@gmail.com>
2025-12-03 23:56:10 -0300

    botoes

--- app/src/main/java/com/example/apphorta/MainActivity.java
+++ app/src/main/java/com/example/apphorta/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.apphorta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMapa, btnVoluntario, btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnMapa = findViewById(R.id.btnMapa);
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Mapa = new Intent(getApplicationContext(), Mapa.class);
+                startActivity(Mapa);
+            }
+        });
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Entrar = new Intent(getApplicationContext(), Entrar.class);
+                startActivity(Entrar);
+            }
+        });
+
+        btnVoluntario= findViewById(R.id.btnVoluntario);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Voluntario = new Intent(getApplicationContext(), Voluntario.class);
+                startActivity(Voluntario);
+            }
         });
     }
 }
